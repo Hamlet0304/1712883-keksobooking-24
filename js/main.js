ec00c67d7295ff188b6ac036836df26a72c239a8
Ghumashyan Hamlet <ghumashyan0304@gmail.com>
2021-10-04 15:16:24 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -9,3 +9,32 @@ const getRandomNumberFromRangeByPrecision = function (from, at, presicion) {
 console.log(getRandomNumberFromRange(10, 15));
 console.log(getRandomNumberFromRangeByPrecision(10, 15, 2));
 console.log(toFixed);
+
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKINS = ['12:00', '13:00', '14:00'];
+const CHECKOUTS = ['12:00', '13:00', '14:00'];
+const FEATURES_ALL = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS_ALL = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+const createOffer = () => {
+  const randomTypeIndex = _.random(0, TYPES.length - 1);
+  const randomCheckinIndex = _.random(0, CHECKINS.length - 1);
+  const randomCheckoutIndex = _.random(0, CHECKOUTS.length - 1);
+  const randomFeatureIndex = _.random(0, FEATURES_ALL.length - 1);
+  const randomPhotoIndex = _.random(0, PHOTOS_ALL.length - 1);
+
+
+  return {
+    type:TYPES[randomTypeIndex],
+    checkin:CHECKINS[randomCheckinIndex] ,
+    checkout:CHECKOUTS[randomCheckoutIndex] ,
+    feature:FEATURES_ALL[randomFeatureIndex] ,
+    photo:PHOTOS_ALL[randomPhotoIndex] ,
+  };
+};
+
+console.log(
+  createOffer()
+);
