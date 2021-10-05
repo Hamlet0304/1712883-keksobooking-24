57d72a879c035cdef48288784705162b68b1c8d7
Ghumashyan Hamlet <ghumashyan0304@gmail.com>
2021-10-06 00:26:34 +0300

    Больше деталей изменения

--- js/main.js
+++ js/main.js
@@ -17,6 +17,18 @@ const FEATURES_ALL = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'co
 const PHOTOS_ALL = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const TITLES = ['rent house',  'rent room'];
+const ROOMS = ['big room', 'small room', 'medium room'];
+const DESCRIPTIONS = ['Сдам номер'];
+
+
+const getAvatarNumber = function () {
+  const randomNumber = getRandomNumberFromRange(0, 10);
+  if (randomNumber < 10) {
+    return `0${randomNumber}`;
+  }
+  return randomNumber;
+};
 
 const createOffer = () => {
   const randomTypeIndex = _.random(0, TYPES.length - 1);
@@ -24,14 +36,34 @@ const createOffer = () => {
   const randomCheckoutIndex = _.random(0, CHECKOUTS.length - 1);
   const randomFeatureIndex = _.random(0, FEATURES_ALL.length - 1);
   const randomPhotoIndex = _.random(0, PHOTOS_ALL.length - 1);
+  const randomTitleIndex = _.random(0, TYPES.length - 1);
+  const randomRoomIndex = _.random(0, TYPES.length - 1);
+  const randomDescriptionIndex = _.random(0, TYPES.length - 1);
+
+  // const name = "Vasya";
+  // const name1 = name;
+  // const age = 32;
+  // const people  = { name: "Vasya", age 32 };
+  // const male = true;
 
 
   return {
+    author: {
+      avatar:`img/avatars/user${getAvatarNumber()}.png`,
+    },
+    price:getRandomNumberFromRange(0, 10000) ,
     type:TYPES[randomTypeIndex],
     checkin:CHECKINS[randomCheckinIndex] ,
     checkout:CHECKOUTS[randomCheckoutIndex] ,
     feature:FEATURES_ALL[randomFeatureIndex] ,
-    photo:PHOTOS_ALL[randomPhotoIndex] ,
+    photo: PHOTOS_ALL[randomPhotoIndex],
+    title: TITLES[randomTitleIndex],
+    room: ROOMS[randomRoomIndex],
+    description : DESCRIPTIONS[randomDescriptionIndex],
+    location: {
+      lng:getRandomNumberFromRangeByPrecision(35.65000 , 35.70000 ,5),
+      lat:getRandomNumberFromRangeByPrecision(139.70000 , 139.80000 , 5),
+    },
   };
 };
 
