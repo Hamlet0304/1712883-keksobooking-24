0a884a2364295737e6d699369df3e620d9b9ab82
Ghumashyan Hamlet <ghumashyan0304@gmail.com>
2021-10-08 14:39:52 +0300

    Модуляция

---
+++ js/data.js
@@ -0,0 +1,13 @@
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKINS = ['12:00', '13:00', '14:00'];
+const CHECKOUTS = ['12:00', '13:00', '14:00'];
+const FEATURES_ALL = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS_ALL = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const TITLES = ['rent house',  'rent room'];
+const ROOMS = ['big room', 'small room', 'medium room'];
+const DESCRIPTIONS = ['Сдам номер'];
+
+
+export { TYPES, CHECKINS, CHECKOUTS, FEATURES_ALL, PHOTOS_ALL, TITLES, ROOMS, DESCRIPTIONS};
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import {TYPES, CHECKINS, CHECKOUTS, FEATURES_ALL, PHOTOS_ALL, TITLES, ROOMS, DESCRIPTIONS} from './data';
+
 const getRandomNumberFromRange = function (from, at) {
   return Math.floor(Math.random() * (from - at + 1)) + at;
 };
@@ -9,17 +11,6 @@ const getRandomNumberFromRangeByPrecision = function (from, at, presicion) {
 console.log(getRandomNumberFromRange(10, 15));
 console.log(getRandomNumberFromRangeByPrecision(10, 15, 2));
 console.log(toFixed);
-
-const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const CHECKINS = ['12:00', '13:00', '14:00'];
-const CHECKOUTS = ['12:00', '13:00', '14:00'];
-const FEATURES_ALL = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const PHOTOS_ALL = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
-const TITLES = ['rent house',  'rent room'];
-const ROOMS = ['big room', 'small room', 'medium room'];
-const DESCRIPTIONS = ['Сдам номер'];
 
 
 const getAvatarNumber = function () {
